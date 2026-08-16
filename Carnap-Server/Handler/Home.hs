473c218d7b6fcf691f24df1138844d40f03d15bc
Greg Johnson <54258105+loighic@users.noreply.github.com>
2026-08-16 17:18:48 -0500

    Update Home.hs

--- Carnap-Server/Handler/Home.hs
+++ Carnap-Server/Handler/Home.hs
@@ -7,5 +7,5 @@ getHomeR = do
     defaultLayout $ do
         --attempt to preload ghcjs runtime
         addScript $ StaticR ghcjs_rts_js
-        setTitle "Welcome To Carnap!"
+        setTitle "Welcome to Carnap: MSU!"
         $(widgetFile "homepage")
